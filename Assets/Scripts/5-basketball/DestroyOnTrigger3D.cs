f2d32fa828cb8f668bd5120af2f81cc7bf360c82
Dvir-Segal <Dvirsegal96@gmail.com>
2023-04-27 01:08:30 +0300

    rnthdgd;;

---
+++ Assets/Scripts/5-basketball/DestroyOnTrigger3D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * This component destroys its object whenever it tringgers a 3D collider with the given tag.
+ */
+public class DestroyOnTrigger3D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy") {
+            Destroy(this.gameObject);
+            // Destroy(other.gameObject);
+        }
+        if (other.tag== "goal") {
+            print("bi");
+            if (SceneManager.GetActiveScene().name=="Ball&Gate_1"){
+                SceneManager.LoadScene("Ball&Gate_2");
+            }
+            else{
+                SceneManager.LoadScene("Ball&Gate_3");
+            }
+                
+        }
+
+    }
+
+    private void Update() {
+
+    }
+}
